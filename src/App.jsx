1100008c2dce52461f2c0af966c0094bc860f706
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2024-01-21 20:35:11 +0300

    10.1 Projenin Hazırlanması

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  const StyledContainer = styled.div`
+    background-color: aqua;
+    padding: 20px;
+  `;
+
+  return (
+    <>
+      <StyledContainer>merhaba</StyledContainer>
+    </>
+  );
+}
+
+export default App;
