1ca212dab626613ba6a7bb4694d4f69a7641a752
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2024-01-22 00:09:46 +0300

    10.6 Animasyon Oluşturma

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { FaCanadianMapleLeaf } from "react-icons/fa";
 import { StyledContainer } from "./components/styled";
 import { Button } from "./components/button";
 import {
@@ -7,12 +7,42 @@ import {
   StrongButton,
   SubmitButton,
 } from "./components/button/Button.styled";
+import styled, { keyframes } from "styled-components";
+
+const rotate = keyframes`
+from{
+  transform: rotate(0deg)
+}
+to{
+  transform: rotate(360deg)
+}
+`;
+
+const CanadaIcon = styled(FaCanadianMapleLeaf)`
+  font-size: 40px;
+  animation: ${rotate} 3s linear infinite;
+  color: #e74c3c;
+`;
+
+const StyledImg = styled.img`
+  width: 100%;
+  padding: 4px;
+  border: 1px solid #bdc3c7;
+  border-radius: 4px;
+  &:hover {
+    animation: ${rotate} 10s linear infinite;
+  }
+`;
 
 function App() {
   return (
     <>
       <StyledContainer>
+        <StyledImg src="https://picsum.photos/id/35/1200/300" alt="" />
+        <hr />
+        <CanadaIcon></CanadaIcon>
         <h1>styled component uygulandı</h1>
+
         <Button $primary>Button</Button>
         <br />
         <StrongButton $primary>Strong Button</StrongButton>
